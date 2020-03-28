99a29cf52b5afde3b371a59bb83c30af982e4da8
Adegoke Adegunloye <gokeonfire@yahoo.com>
2020-03-28 17:31:27 +0100

    Day0 solution

--- Day0/Day0/Program.cs
+++ Day0/Day0/Program.cs
@@ -10,6 +10,15 @@ namespace Day0
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(GetDayAndTime());
+        }
+
+        static string GetDayAndTime()
+        {
+            string day = DateTime.Now.DayOfWeek.ToString();
+            string time = DateTime.Now.ToLongTimeString();
+
+            return String.Format("Today is {0}\nCurrent time is {1}", day, time);
         }
     }
 }
